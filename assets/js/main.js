ba7f9a0e894a880e771ee7214767660ab5abed17
biplavpaudel123 <biplavpaudel123@gmail.com>
2025-08-01 23:02:30 +0545

    remove contact reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,13 +48,13 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 1000,
     delay: 100,
-         reset: false // chnage to show scroll animation behaviour again  
+         reset: true // chnage to show scroll animation behaviour again  
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 200 });
 sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.skills__data, .work__img, .contact__input', { interval: 20 });
+sr.reveal('.skills__data, .work__img', { interval: 20 });
 
 
 let lastX = 0;
